5469cd32eff4fcb48bb5a19216af09731d29c3d3
heart-joy <chenhanrao@gmail.com>
2025-09-28 20:34:50 +0800

    update

---
+++ time_transformer.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+  int n;
+  int h, m；
+  printf("请输入分钟数n:");
+  scanf("%d", &n);
+  h = n / 60;
+  m = n % 60;
+  printf("%d分钟可换算为：%d小时%d分钟\n", n, h, m);
+  return 0;
+}
